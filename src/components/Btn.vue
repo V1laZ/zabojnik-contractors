9fcdcc8729e35c7286b8341c0c59bb339cd6c734
V1laZ <pasikstudiocz@gmail.com>
2022-09-12 11:57:14 +0200

    button styling

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,32 +1,35 @@
 <template>
-    <button type="button" class="btn btn-secondary btn-lg">
+    <button type="button" class="my-btn">
         {{ text }}
     </button>
 </template>
 
 <script>
-    export default {
-        name: 'BtnComponetn',
-        props: {
-            text: String
-        }
+export default {
+    name: 'BtnComponetn',
+    props: {
+        text: String
     }
+}
 </script>
 
 <style scoped>
-.btn {
+.my-btn {
     border-radius: 5rem;
     color: white;
-}
-
-.btn:hover {
-    background-color: #04AA6D;
-}
-
-.btn-secondary {
-    background-color: rgb(83, 83, 83);
+    background-color: grey;
+    font-size: 1.25rem;
+    padding: 0.5rem;
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
     border: none;
+    background-color: rgb(83, 83, 83);
+    transition: 0.2s background-color ease;
 }
 
+.my-btn:hover {
+    background-color: #04AA6D;
+    transition: 0.2s background-color ease;
+}
 
 </style>
