798b461b6631ce0610018f814dbf22fe5cde267e
V1laZ <pasikstudiocz@gmail.com>
2022-09-12 11:57:40 +0200

    cleaner code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,23 @@
 <template>
-    <div class="text-light mt-5">
-        <div>
-            <svg width="83.964mm" height="60.787mm" version="1.1" viewBox="0 0 73.964 50.787" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-70.179 -53.735)"><text x="70.277176" y="93.006042" fill="#ffffff" font-family="'Good Times'" font-size="11.537px" stroke="#000000" stroke-linecap="square" stroke-width="0" style="paint-order:stroke fill markers" xml:space="preserve"><tspan x="70.277176" y="93.006042" fill="#ffffff" font-family="'Good Times'" font-size="11.537px" stroke-width="0">ZÁBOJNÍK</tspan></text><g class="UnoptimicedTransforms" transform="matrix(1.4155 0 0 1.3981 40.875 -13.569)"><path d="m21.202 67.301h19.571l-4.6226-8.0066 7.1065 4.1029 3.5708-13.326 3.5708 13.326 7.1065-4.1029-4.6226 8.0066h19.571" fill="none" stroke="#008000" stroke-linecap="square" style="paint-order:stroke fill markers"/></g><g transform="matrix(1.2197 0 0 1 49.923 -22.287)"><rect x="16.648" y="116.96" width="60.559" height="9.8446" fill="#008000" stroke-width="0" style="paint-order:stroke fill markers"/></g><text transform="matrix(.11122 0 0 .11122 55.012 51.348)" fill="#ffff00" font-family="'Good Times'" font-size="66.667px" stroke="#000000" stroke-linecap="square" stroke-width="0" style="paint-order:stroke fill markers;shape-inside:url(#rect2795-8-74-5);white-space:pre" xml:space="preserve"><tspan x="152.2168" y="458.57269">CONTRACTORS</tspan></text></g></svg>
+    <div class="mt-5">
+        <div class="d-flex justify-content-center d-sm-none">
+            <Logo  width="335" />
+        </div>
+        <div class="d-none d-sm-block">
+            <Logo  width="335" />
         </div>
         <Transition>
-            <Btn v-if=btnIsVisible text="E-shop" class="mt-4" />
+            <Btn v-if=btnIsVisible text="E-shop" class="mt-5" />
         </Transition>
     </div>
 </template>
 
 <script>
 import Btn from './Btn.vue';
+import Logo from './Logo.vue';
 export default {
     name: "HeaderComponent",
-    components: { Btn },
+    components: { Btn, Logo },
     props: {
         btnIsVisible: Boolean
     },
